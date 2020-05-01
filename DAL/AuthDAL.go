a2004e3333e6ced4503aa2da194c799c8b6533ba
Varun Ojha <vo383@nyu.edu>
2020-05-01 01:32:03 -0400

    fixed auth dal

--- DAL/AuthDAL.go
+++ DAL/AuthDAL.go
@@ -2,19 +2,50 @@ package DAL
 
 import "ds-project/config"
 
-func SetAccessToken(appConfig *config.ApplicationConfig, username string, token string) bool {
-	appConfig.Tokens[username] = token
+type TokenDB struct {
+	Tokens        map[string]string
+}
+
+func SetAccessToken(ctx context.Context, kv clientv3.KV, username string, token string) bool {
+	bt := RAFT.GetKey(ctx,kv,"tokens")
+
+	var result TokenDB
+    err:= json.Unmarshal(bt, &result)
+    if err != nil {
+        panic(err)
+    }
+
+    result.Tokens[username] = token
+	marshalledToken, err := json.Marshal(result)
+	RAFT.PutKey(ctx,kv,"tokens",marshalledToken)
 	return true
 }
 
-func GetAccessToken(appConfig *config.ApplicationConfig, username string) (string, bool) {
-	token, ok := appConfig.Tokens[username]
+func GetAccessToken(ctx context.Context, kv clientv3.KV, username string) (string, bool) {
+	bt := RAFT.GetKey(ctx,kv,"tokens")
+
+	var result TokenDB
+    err:= json.Unmarshal(bt, &result)
+    if err != nil {
+        panic(err)
+    }
+
+	token, ok := result.Tokens[username]
 	return token, ok
 }
 
-func DeleteAccessToken(appConfig *config.ApplicationConfig, username string) bool {
-	if _, ok := appConfig.Tokens[username]; ok {
-		delete(appConfig.Tokens, username)
-	}
+func DeleteAccessToken(ctx context.Context, kv clientv3.KV, username string) bool {
+	bt := RAFT.GetKey(ctx,kv,"tokens")
+
+	var result TokenDB
+    err:= json.Unmarshal(bt, &result)
+    if err != nil {
+        panic(err)
+    }
+
+   	delete(result.Tokens, username)
+
+	marshalledToken, err := json.Marshal(result)
+	RAFT.PutKey(ctx,kv,"tokens",marshalledToken)
 	return true
 }
