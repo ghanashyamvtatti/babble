d50eb2ff3d113f66643cba2e5c0540e2a6e64ea2
Varun Ojha <vo383@nyu.edu>
2020-05-03 03:46:42 -0400

    a commit

--- common/utilities/etcdhelper.go
+++ common/utilities/etcdhelper.go
@@ -3,13 +3,18 @@ package utilities
 import (
 	"context"
 	"github.com/coreos/etcd/clientv3"
+	"log"
 )
 
 func GetKey(ctx context.Context, client *clientv3.Client, key string) []byte {
 	response, _ := client.Get(ctx, key)
 	var bytes []byte
+	log.Println("In get key")
+	log.Println(response)
+	if response != nil{
 	if response.Kvs != nil {
 		bytes = []byte(string(response.Kvs[0].Value))
+	}
 	}
 	return bytes
 }
